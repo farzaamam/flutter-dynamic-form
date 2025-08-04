8ac5d86c4a460e9f3a31d20d31870a00b979e6b9
farzam <farzaamam1995@gmail.com>
2025-08-04 13:05:02 +0330

    Add fetchForm Logic in controller

--- lib/presentation/form_controller.dart
+++ lib/presentation/form_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_dynamic_form/di/global_providers.dart';
 import 'package:flutter_dynamic_form/domain/form_repository.dart';
 import 'package:flutter_dynamic_form/domain/models/form_input_field_models.dart';
@@ -13,5 +15,22 @@ final formControllerProvider =
 class FormController extends StateNotifier<AsyncValue<List<InputField>>> {
   final FormRepository formRepository;
 
-  FormController(this.formRepository) : super(const AsyncLoading());
+  FormController(this.formRepository) : super(const AsyncLoading()) {
+    _init();
+  }
+
+  Future<void> _init() async {
+    await _load();
+  }
+
+  Future<void> _load() async {
+    state = const AsyncLoading();
+    try {
+      final forms = await formRepository.getForms();
+      state = AsyncValue.data(forms);
+    } catch (e, st) {
+      state = AsyncValue.error(e, st);
+      debugPrint('Failed to load: $e\n$st');
+    }
+  }
 }
