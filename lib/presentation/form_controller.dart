965079848efcda2221b52f5ac5f54bead243848a
farzam <farzaamam1995@gmail.com>
2025-08-04 19:22:07 +0330

    Add submit form

--- lib/presentation/form_controller.dart
+++ lib/presentation/form_controller.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_dynamic_form/di/global_providers.dart';
 import 'package:flutter_dynamic_form/domain/form_repository.dart';
 import 'package:flutter_dynamic_form/domain/models/form_input_field_models.dart';
+import 'package:flutter_dynamic_form/presentation/widgets/file_field_widget.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final formControllerProvider =
@@ -33,4 +34,37 @@ class FormController extends StateNotifier<AsyncValue<List<InputField>>> {
       debugPrint('Failed to load: $e\n$st');
     }
   }
+  load(){}
+
+  bool isSubmitting = false;
+  final Map<String, dynamic> fieldValues = {};
+  final Map<String, UploadedFile?> files = {};
+
+  // Other existing members...
+
+  void updateFieldValue(String title, dynamic value) {
+    fieldValues[title] = value;
+  }
+
+  void updateFile(String title, UploadedFile? file) {
+    if (file != null) {
+      files[title] = file;
+    } else {
+      files.remove(title);
+    }
+  }
+
+  Future<void> submit() async {
+    isSubmitting = true;
+    try {
+
+    } catch (e,s) {
+      print("dfesfdrgvdrv $s : $e");
+
+      rethrow;
+    } finally {
+      isSubmitting = false;
+    }
+  }
+
 }
